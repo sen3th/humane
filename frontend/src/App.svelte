d63d35de884abff521ce129bbe512802d28ab121
Seneth <126297209+sen3th@users.noreply.github.com>
2026-04-16 12:32:56 -0400

    spinner spinning with text fix

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -524,10 +524,10 @@
     <div class="wake-background">
       <div class="wake-card">
         <div class="wake-spin-thing">
-          <h3>Connecting to backend</h3> 
+        </div>
+        <h3>Connecting to backend</h3> 
           <p>{wakeMessage}</p>
           <p class="wake-sub">first request can take a bit of time.</p>
-        </div>
       </div>
     </div>
     {/if}
